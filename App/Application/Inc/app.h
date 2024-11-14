b55b250d84790b7b06521957652bfff909f17451
Alisson Evangelista <alissongiovane22@gmail.com>
2024-11-13 21:59:47 -0300

    app.c / app.h corrigido

--- App/Application/Inc/app.h
+++ App/Application/Inc/app.h
@@ -18,10 +18,15 @@ extern volatile uint32_t app_execution_rate_1ms_timer;
 
 
 /******************************************************************************/
+// AlissonGOE
 
 void transit_state(uint16_t time_between_loops_t, uint16_t time_in_loop_t, uint16_t gap, uint16_t time_spent_in_the_bonds_t);
 void current_status(traffic_status_t state);
 void traffic_calculation_app(void);
+
+// AlissonGOE
+/******************************************************************************/
+
 void app_1ms_clock(void);
 void app_init(void);
 void app_update(void);
