bec46587e2d38c3d586e7c09561e6bfb30f36c44
MV Tech <136182053+MarcusViniciusTEC@users.noreply.github.com>
2024-11-14 08:09:38 -0300

    axles funcionando

--- App/Application/Src/app.c
+++ App/Application/Src/app.c
@@ -33,39 +33,25 @@ uint16_t gap_traffic = 0;
 #define DELAY_BETWEEN_CARS 1000  // 1000ms de espera entre carros
 
 
-typedef struct {
-    uint32_t delay_time;  
-    uint8_t led_index;    
-    uint8_t active;       
-} axle_t;
 
 
-typedef struct {
-    axle_t* axles;       
-    uint8_t num_axles;    
-} traffic_t;
 
-
-axle_t car1_axles[9];   
-
-traffic_t traffic[1];          
-
+         
 uint32_t car_delay_timer = 0;  
-uint8_t current_car = 0;       
+uint8_t current_car = 0; 
+traffic_t traffic[1];
+axle_t axles[9];         
 
+void init_axles(void) 
+{
+    
+     
 
-void init_axles(void) {
-    traffic[0].axles = car1_axles;
-    traffic[0].num_axles = 9;
+    traffic[0].axles = axles;
+    traffic[0].num_axles = 3;
     traffic[0].axles[0].delay_time = 1;
-    traffic[0].axles[1].delay_time = 5;
-    traffic[0].axles[2].delay_time = 6;
-    traffic[0].axles[3].delay_time = 10;
-    traffic[0].axles[4].delay_time = 11;
-    traffic[0].axles[5].delay_time = 15;
-    traffic[0].axles[6].delay_time = 16;
-    traffic[0].axles[7].delay_time = 21;
-    traffic[0].axles[8].delay_time = 22;
+    traffic[0].axles[1].delay_time = 15;
+    traffic[0].axles[2].delay_time = 25;
 
     for (int i = 0; i < traffic[current_car].num_axles; i++) 
     {
@@ -75,42 +61,19 @@ void init_axles(void) {
 
 
 void app_1ms_clock(void) {
+
     static uint32_t timer = 0;
 
-    if (car_delay_timer > 0) 
+    for (uint8_t i = 0; i < traffic[current_car].num_axles; i++) 
     {
-        car_delay_timer--;
-        return;  
-    }
-
-    for (int i = 0; i < traffic[current_car].num_axles; i++) 
-    {
-        if (traffic[current_car].axles[i].active && timer >= traffic[current_car].axles[i].delay_time) 
+        if (traffic[current_car].axles[i].active && timer == traffic[current_car].axles[i].delay_time) 
         {
             hmi_led_turn_on(traffic[current_car].axles[i].led_index);
             hmi_led_turn_off(traffic[current_car].axles[i].led_index);
             traffic[current_car].axles[i].active = 0;  
         }
     }
-    uint8_t all_axles_done = 1;
-    for (int i = 0; i < traffic[current_car].num_axles; i++) 
-    {
-        if (traffic[current_car].axles[i].active) 
-        {
-            all_axles_done = 0;
-            break;
-        }
-    }
-    if (all_axles_done) 
-    {
-        car_delay_timer = DELAY_BETWEEN_CARS;  
-        current_car++;
-        if (current_car >= 1) 
-        {
-            current_car = 0;  
-        }
-        init_axles();  
-    }
+
     timer++;
 }
 
@@ -271,16 +234,12 @@ void current_status(traffic_status_t state)
 // AlissonGOE
 /******************************************************************************/
 
-void app_1ms_clock(void)
-{
-  
-}
 
 /******************************************************************************/
 
 void app_init(void)
 {
-         
+         init_axles();
 }
 
 /******************************************************************************/
